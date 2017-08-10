74d506a2d5011186ff48161cea506c6ec61ae17b
sravan <sravan.rathnam@gmail.com>
2017-08-10 10:34:21 +0530

    code committed by sravs

---
+++ gitbuilddemo/src/gitbuildtest/printdemo.java
@@ -0,0 +1,12 @@
+package gitbuildtest;
+
+public class printdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Git hub integration11");
+		
+	}
+
+}
